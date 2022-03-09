f22ef21b23eb77e23c4985569521cbc7ec10d1cc
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-09 21:27:10 +0530

    Time: 56 ms (27.31%), Space: 60 MB (9.75%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    TreeMap<Integer,Integer> tm;
+    int sum;
+    Random rand = new Random();
+    public Solution(int[] w) {
+        tm = new TreeMap<>();
+        sum =0;
+        for(int i=0; i<w.length; i++)
+        {
+            sum = sum + w[i];
+            tm.put(sum,i);
+        }
+        
+        
+    }
+    
+    public int pickIndex() {
+       int num = rand.nextInt(sum)+1;
+       return tm.ceilingEntry(num).getValue();
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
