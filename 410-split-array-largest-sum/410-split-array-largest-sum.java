66a76648422066458a32f56c6d4a3159fdad9048
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-10 07:53:46 +0530

    Time: 1 ms (84.89%), Space: 42.1 MB (21.15%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,49 +1,45 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-       int sum =0;
-       int max=0;
-       for(int i=0; i<nums.length; i++)
-       {
-           sum = sum+nums[i];
-           max = Math.max(max, nums[i]);
-       }
-       int l=max;
-       int r=sum;
-        System.out.println(" l :"+l +", r:"+ r);
-       while(l<r)
-       {
-           
-           int mid = l+(r-l)/2;
-           int cnt = findPeople(nums,mid);
-           //System.out.println(" l :"+l +", r:"+ r + ", cnt :" + cnt);
-           if(cnt>m)
-           {
-               l =mid+1;
-           }
-           else
-           {
-               r = mid;
-           }
-       }
+        int sum=0, max =0;
+        for(int i=0; i<nums.length; i++)
+        {
+            sum = sum+nums[i];
+            max = Math.max(max, nums[i]);
+        }
         
-       return r;
+        int l=max;
+        int r= sum;
+        
+        while(l<r)
+        {
+            int mid = l +(r-l)/2;
+            int cnt = findPeople(nums, mid);
+            if(cnt > m)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        return r;
         
     }
     
     public int findPeople(int[] arr, int val)
     {
-        int sum =0;
-        int cnt=1;
+        int cnt=1, sum =0;
         for(int i=0; i<arr.length; i++)
         {
-            if(sum+arr[i] <= val)
-            {
-                sum = sum+arr[i];
-            }
+           if(sum+arr[i] <= val)
+           {
+              sum += arr[i]; 
+           }
             else
             {
-                sum = arr[i];
                 cnt++;
+                sum =arr[i];
             }
         }
         return cnt;
