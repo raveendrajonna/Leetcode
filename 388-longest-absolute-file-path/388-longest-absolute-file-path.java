528c910d51d0cb93cc937a7d6d724bd2f940b301
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-10 06:44:54 +0530

    Time: 1 ms (94.08%), Space: 41.8 MB (42.78%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.java
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.java
@@ -1,24 +1,21 @@
 class Solution {
     public int lengthLongestPath(String input) {
-        Stack<Integer> stk =new Stack<>();
+        Stack<Integer> stk = new Stack<>();
         stk.push(0);
         int res=0;
         for(String s : input.split("\n"))
         {
             int level = s.lastIndexOf("\t")+1;
-            while(stk.size()> level+1)
+            while(stk.size() > level+1)
             {
                 stk.pop();
             }
             
-            int curLen = stk.peek()+s.length()-level+1;
+            int curLen =  stk.peek()+s.length()-level+1;
             stk.push(curLen);
             if(s.contains("."))
-            {
                 res = Math.max(res, curLen-1);
-            }
         }
-        
         return res;
     }
 }
