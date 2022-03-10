c7ecc82b5e73dc91fdbe81e2843f8b5fd0e82d2a
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-10 23:18:25 +0530

    Time: 1059 ms (9.61%), Space: 118.9 MB (23.68%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.java
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.java
@@ -1,45 +1,34 @@
 class Solution {
     public int minAreaRect(int[][] points) {
-        HashMap<String,int[]> hm = new HashMap<>();
+        HashSet<String> hs = new HashSet<>();
         int res = Integer.MAX_VALUE;
-        for(int i=0; i<points.length; i++)
+        for(int i=0; i< points.length; i++)
         {
-            String key = points[i][0]+"_"+points[i][1];
-            hm.put(key, points[i]);
+            String key = points[i][0]+"-"+points[i][1];
+            hs.add(key);
         }
         
-        
-        
-        for(int i=0; i<points.length; i++)
+        for(int i=0; i < points.length; i++)
         {
-            for(int j=i+1; j<points.length; j++)
+            for(int j=i+1; j< points.length; j++)
             {
                 int x1 = points[i][0];
                 int y1 = points[i][1];
                 int x2 = points[j][0];
                 int y2 = points[j][1];
                 
-                if(x1 == x2 || y1 == y2)
-                {
+                if(x1 == x2 || y2 == y1)
                     continue;
-                }
                 
-                String point3 = x1+"_"+y2;
-                String point4 = x2+"_"+y1;
-                
-                if(hm.containsKey(point3) && hm.containsKey(point4))
+                if(hs.contains(x1+"-"+y2) && hs.contains(x2+"-"+y1))
                 {
-                    
-                    //rectangle found, calculate area
-                    int  vlen = Math.abs(y2-y1);
-                    int  hlen = Math.abs(x2-x1);
-                    res = Math.min(res, vlen*hlen);
-                    //System.out.println(" x1 :"+ x1 +", x2 :" + x2 +", y1 :" +y1 +", y2 :" + y2 +", res :" + res);
+                   res = Math.min(res, Math.abs(x2-x1) * Math.abs(y2-y1));
                 }
             }
         }
         if(res == Integer.MAX_VALUE)
             return 0;
+        
         return res;
     }
 }
