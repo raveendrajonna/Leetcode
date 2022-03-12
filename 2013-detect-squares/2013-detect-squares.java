a4ca32578e00c445a334915248210c46de69d936
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-12 11:35:01 +0530

    Time: 329 ms (13.91%), Space: 108.7 MB (48.88%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,7 +1,5 @@
 class DetectSquares {
-
-    HashMap<String,Integer> hm;
-
+   HashMap<String, Integer> hm ;
     List<int[]> points;
     public DetectSquares() {
         hm = new HashMap<>();
@@ -15,19 +13,18 @@ class DetectSquares {
     }
     
     public int count(int[] point) {
-        int res=0;
-      for(int[] p : points)
-      {
-          int x1= point[0];
-          int y1 = point[1];
-          int x2 = p[0];
-          int y2=p[1];
-          
-          if(x1== x2 || y1 == y2 || Math.abs(x1-x2) != Math.abs(y1-y2))
-              continue;
-          
-          res = res + (hm.getOrDefault(x1+"-"+y2,0) * hm.getOrDefault(x2+"-"+y1,0));
-      }
+      int res =0;
+      int x1 = point[0];
+      int y1 = point[1];
+        
+        for(int[] p : points)
+        {
+            int x2 = p[0];
+            int y2 = p[1];
+            if(x1 == x2 || y1 == y2 || Math.abs(x1-x2) != Math.abs(y1-y2))
+                continue;
+            res = res + hm.getOrDefault(x1+"-"+y2,0)*hm.getOrDefault(x2+"-"+y1,0);
+        }
         return res;
     }
 }
