9daeca89c978b73526e5d8a7bc2eefdb0e1e5912
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-12 14:46:04 +0530

    Time: 8 ms (82.47%), Space: 42.2 MB (11.36%) - LeetHub

--- 418-sentence-screen-fitting/418-sentence-screen-fitting.java
+++ 418-sentence-screen-fitting/418-sentence-screen-fitting.java
@@ -3,14 +3,14 @@ class Solution {
         String str = String.join(" ",sentence)+" ";
         int len = str.length();
         int cur=0;
+        
         for(int i=0; i<rows; i++)
         {
-            cur = cur+cols;
-            while(cur>=0 && str.charAt(cur%len) != ' ')
+           cur = cur+cols;
+            while(cur>=0 && str.charAt(cur%len) !=' ')
             {
                 cur--;
             }
-            
             cur++;
         }
         return cur/len;
