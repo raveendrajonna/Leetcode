36b71cd8764777ee32c0fbf58088c7b7661f4a2a
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-13 08:37:22 +0530

    Time: 60 ms (36.99%), Space: 44 MB (48.25%) - LeetHub

---
+++ 664-strange-printer/664-strange-printer.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int strangePrinter(String s) {
+        int n= s.length();
+        int[][] dp = new int[n][n];
+        
+        for(int i=0; i<n; i++)
+        {
+            dp[i][i] = 1;
+        }
+        
+        for(int i=0; i<n-1; i++)
+        {
+            dp[i][i+1] = s.charAt(i) == s.charAt(i+1)? 1:2;
+        }
+        
+        for(int len=3; len <= n; len++)
+        {
+            for(int i=0; i+len <= n ;i++)
+            {
+                int j = i+len-1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<j; k++)
+                {
+                  int val = dp[i][k] + dp[k+1][j];
+                    
+                 val =  s.charAt(i) == s.charAt(j) ? val-1 : val;
+                    dp[i][j] = Math.min(dp[i][j], val);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+}
