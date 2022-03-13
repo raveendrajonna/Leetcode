af632d1b8a5842515e3c6cdd511e54e244ad80ba
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-13 19:58:11 +0530

    Time: 32 ms (88.44%), Space: 46.9 MB (87.89%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.java
+++ 721-accounts-merge/721-accounts-merge.java
@@ -1,51 +1,53 @@
 class Solution {
-    HashMap<String,String> hmEmailToName;
     HashMap<String,Integer> hmEmailToId;
-    int id=0;
+    HashMap<String,String> hmEmailToName;
     DS ds;
-    
+    int id=0;
+    HashMap<Integer, List<String>> hmIdToEmails;
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-
-        ds = new DS();
         hmEmailToId = new HashMap<>();
         hmEmailToName = new HashMap<>();
+        ds = new DS();
+        hmIdToEmails = new HashMap<>();
+        
         for(List<String> account : accounts)
         {
             String name = account.get(0);
+            
             for(int i=1; i<account.size(); i++)
             {
+                if(!hmEmailToName.containsKey(account.get(i)))
+                  hmEmailToName.put(account.get(i) , name);
+                
                 if(!hmEmailToId.containsKey(account.get(i)))
                 {
                     hmEmailToId.put(account.get(i), id);
                     id++;
                 }
-                hmEmailToName.put(account.get(i), name);
-                ds.merge(hmEmailToId.get(account.get(1)), hmEmailToId.get(account.get(i)));
+                
+                ds.merge(hmEmailToId.get(account.get(1)) , hmEmailToId.get(account.get(i)));
             }
         }
         
-        HashMap<Integer, List<String>> res = new HashMap<>();
         for(String email : hmEmailToId.keySet())
         {
-            int idx =  ds.find(hmEmailToId.get(email));
-            if(!res.containsKey(idx))
-                res.put(idx, new ArrayList<>() );
-            
-            res.get(idx).add(email);
+            int idx = ds.find(hmEmailToId.get(email));
+            if(!hmIdToEmails.containsKey(idx))
+            {
+                hmIdToEmails.put(idx, new ArrayList<>());
+            }
+            hmIdToEmails.get(idx).add(email);
         }
         
-        List<List<String>> fr = new ArrayList<>(res.values());
-        for(List<String> r : fr)
+        for(List<String> lst : hmIdToEmails.values())
         {
-            Collections.sort(r);
-            r.add(0, hmEmailToName.get(r.get(0)));
+            Collections.sort(lst);
+            lst.add(0, hmEmailToName.get(lst.get(0)));
         }
         
+        return new ArrayList(hmIdToEmails.values());
         
         
-        return fr;
-        
-    
     }
     
     class DS
@@ -54,8 +56,8 @@ class Solution {
         
         public DS()
         {
-            parent = new int[10000];
-            for(int i=0; i< 10000; i++)
+            parent = new int[10001];
+            for(int i=0; i<10001; i++)
             {
                 parent[i] = i;
             }
