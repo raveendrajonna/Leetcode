1dff9e4f78f7cdd23af181cf9fc430424d1880c3
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-15 00:19:32 +0530

    Time: 1711 ms (5.00%), Space: 42 MB (17.61%) - LeetHub

--- 465-optimal-account-balancing/465-optimal-account-balancing.java
+++ 465-optimal-account-balancing/465-optimal-account-balancing.java
@@ -1,37 +1,33 @@
 class Solution {
     public int minTransfers(int[][] transactions) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
+      HashMap<Integer,Integer> hm = new HashMap<>();
         for(int[] transaction : transactions)
         {
-            hm.put(transaction[0], hm.getOrDefault(transaction[0],0)+ transaction[2]);
-            hm.put(transaction[1], hm.getOrDefault(transaction[1],0)- transaction[2]);
+            hm.put(transaction[0], hm.getOrDefault(transaction[0], 0) + transaction[2]);
+            hm.put(transaction[1], hm.getOrDefault(transaction[1], 0) - transaction[2]);
         }
-        
+       
         int[] res = new int[hm.size()];
-        int idx=0;
-        for(int key : hm.keySet())
+        int idx =0;
+        for(int value : hm.values())
         {
-            res[idx++] = hm.get(key);
+            res[idx++] = value;
         }
-        
         return dfs(0, res);
     }
     
-    public int dfs(int idx, int[] res)
+    private int dfs(int idx, int[] res)
     {
-       // System.out.println(Arrays.toString(res));
         if(idx == res.length) return 0;
         
-        if(res[idx] == 0)
-            return dfs(idx+1, res);
+        if(res[idx] == 0) return dfs(idx+1, res);
         
         int ans = Integer.MAX_VALUE;
-        
-        for(int i= idx+1; i<res.length; i++)
+        for(int i=idx+1; i <res.length; i++)
         {
-            res[i] += res[idx];
-            ans = Math.min(ans , 1+dfs(idx+1, res));
-            res[i] -= res[idx];
+            res[i]+= res[idx];
+            ans = Math.min(ans, 1+ dfs(idx+1, res));
+            res[i]-= res[idx];
         }
         return ans;
     }
