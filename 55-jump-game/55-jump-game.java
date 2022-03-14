851c812a515f3c9f3cd30d1dc26e0fcb44048951
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-14 16:08:47 +0530

    Time: 3 ms (64.66%), Space: 68.4 MB (25.87%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReachable = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(i>maxReachable) return false;
+            maxReachable = Math.max(maxReachable, i+ nums[i]);
+        }
+        return true;
+    }
+}
