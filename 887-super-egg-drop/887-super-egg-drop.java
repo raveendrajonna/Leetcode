8dab41dd9a00367e254af9cd89c3b3cd5053432e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-14 19:44:01 +0530

    Time: 25 ms (59.22%), Space: 53.9 MB (36.98%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.java
+++ 887-super-egg-drop/887-super-egg-drop.java
@@ -1,9 +1,19 @@
 class Solution {
-    public int superEggDrop(int K, int N) {
-        int dp[] = new int[K + 1], m = 0;
-        for (m = 0; dp[K] < N; ++m)
-            for (int k = K; k > 0; --k)
-                dp[k] += dp[k - 1] + 1;
+    public int superEggDrop(int k, int n) {
+        int[][] dp = new int[n+1][k+1];
+        int m=0;
+        
+        while(dp[m][k] < n)
+        {
+            m++;
+
+                for(int j=1; j<=k; j++)
+                {
+                   dp[m][j] = dp[m-1][j-1]+ dp[m-1][j]+1;  
+                }
+        }
         return m;
+        
+           
     }
 }
