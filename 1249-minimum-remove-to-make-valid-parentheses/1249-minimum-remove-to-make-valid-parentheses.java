9d848e73c00be8ee08abed8e9cde473396cc1aa5
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-15 19:04:22 +0530

    Time: 105 ms (8.28%), Space: 117.2 MB (5.01%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,35 +1,33 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-       Stack<Integer> stk = new Stack<>();
-       StringBuilder sb = new StringBuilder();
-       for(int i=0; i< s.length(); i++)
-       {
-           char c = s.charAt(i);
-           if(c == '(')
-           {
-               stk.push(sb.length());
-               sb.append(c);
-           }
-           else if(c == ')')
-           {
-               if(stk.size() > 0)
-               {
-                   stk.pop();
-                   sb.append(c);
-               }
-               
-           }
-           else 
-           {
-               sb.append(c);
-           }
-       }
-       
-       String res = sb.toString();
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stk = new Stack<>();
+        for(char c : s.toCharArray())
+        {
+            if(c == '(')
+            {
+                stk.push(sb.length());
+                sb.append(c);
+            }
+            else if(c == ')')
+            {
+                if(stk.size() > 0)
+                {
+                    stk.pop();
+                    sb.append(c);
+                }
+            }
+            else
+            {
+                sb.append(c);
+            }
+        }
+        
+        String res = sb.toString();
         while(stk.size() > 0)
         {
             int idx = stk.pop();
-            res = res.substring(0,idx)+ res.substring(idx+1);
+            res = res.substring(0, idx)+ res.substring(idx+1);
         }
         return res;
     }
