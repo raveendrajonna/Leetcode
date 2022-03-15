c3be893eb8de4d3f2424870529f09c7e2abb5a5e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-15 09:21:00 +0530

    Time: 47 ms (77.51%), Space: 66.6 MB (74.57%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canReorderDoubled(int[] arr) {
-      TreeMap<Integer,Integer> tm = new TreeMap<>();
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
         for(int i=0; i<arr.length; i++)
         {
             tm.put(arr[i], tm.getOrDefault(arr[i],0)+1);
@@ -8,19 +8,18 @@ class Solution {
         
         for(int key : tm.keySet())
         {
+            int want = key>0 ? 2 *key : key/2;
+            
             if(tm.get(key) == 0) continue;
             
-            int want = key > 0 ? 2*key : key/2;
-            
-            if(key < 0 && key % 2 != 0) // its negative value, and visited all the doubles, so it would have to update to zero
+            //System.out.println("key/%2 " + (key%2));
+            if(key < 0 && (key%2) != 0)
                 return false;
             
             if(tm.get(key) > tm.getOrDefault(want,0))
-            {
                 return false;
-            }
             
-            tm.put(want, tm.get(want)-tm.get(key));
+            tm.put(want, tm.get(want)- tm.get(key));
         }
         return true;
     }
