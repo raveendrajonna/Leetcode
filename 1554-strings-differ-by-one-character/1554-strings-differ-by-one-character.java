b49d9e62bb09d2ef9e94e6ba8778e0924782f33e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-16 06:32:26 +0530

    Time: 1372 ms (5.02%), Space: 210.3 MB (5.94%) - LeetHub

---
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
@@ -0,0 +1,71 @@
+class Solution {
+    Trie root;
+    public boolean differByOne(String[] dict) {
+        root = new Trie();
+        for(String word : dict)
+        {
+          if(search(word, 0, root, 0) )
+          {
+              return true;
+          }
+           addToTrie(word);
+        } 
+        return false;
+    }
+    
+    public boolean search(String word, int idx, Trie node, int misMatches)
+    {
+       if(misMatches > 1)
+            return false;
+        
+        if(idx == word.length())
+             return true;
+        
+        for(char c : node.childs.keySet())
+        {
+            if(c == word.charAt(idx))
+            {
+                boolean val =  search(word, idx+1, node.childs.get(c), misMatches);
+                if(val) 
+                    return true;
+            }
+            else
+            {
+               boolean val = search(word, idx+1, node.childs.get(c), misMatches+1);
+                if(val)
+                    return true;
+            }
+            
+        }
+        
+        return false;
+    }
+    
+    
+    public void addToTrie(String word)
+    {
+
+              Trie temp = root;
+              for(char c : word.toCharArray())
+              {
+                  if(!temp.childs.containsKey(c))
+                  {
+                      temp.childs.put(c,new Trie());
+                  }
+                  temp = temp.childs.get(c);
+              }
+              temp.isEnd = true;
+    }
+    
+    class Trie
+    {
+        HashMap<Character,Trie> childs;
+        boolean isEnd;
+        
+        public Trie()
+        {
+            childs = new HashMap<>();
+            isEnd = false;
+        }
+    }
+}
