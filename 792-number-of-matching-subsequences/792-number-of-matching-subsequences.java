e2d56bbbf90c5a9ba82fc213db33c72cdcf7ee3e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-17 16:53:52 +0530

    Time: 111 ms (73.81%), Space: 104.1 MB (46.87%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,24 +1,24 @@
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
-        HashMap<Character,Queue<String>> hm = new HashMap<>();
-        for(int i=0; i<26; i++)
+        HashMap<Character, Queue<String>> hm = new HashMap<>();
+        int res=0;
+        for(int i=0; i < 26; i++)
         {
-            hm.put((char)('a'+i), new LinkedList<>());
+            hm.put( (char) ('a'+i), new LinkedList<>());
         }
         
         for(String word : words)
         {
-
             hm.get(word.charAt(0)).add(word);
         }
-        int res=0;
+        
         for(char c : s.toCharArray())
         {
             Queue<String> q = hm.get(c);
             int size = q.size();
             while(size > 0)
             {
-                size--;
+                size--;  
                 String entry = q.remove();
                 if(entry.length() == 1)
                 {
@@ -26,11 +26,10 @@ class Solution {
                 }
                 else
                 {
-                    String newWord = entry.substring(1);
-                    hm.get(newWord.charAt(0)).add(newWord);
-                }
+                    hm.get(entry.charAt(1)).add(entry.substring(1));
+                }          
             }
-          }
-        return res;
         }
+        return res;
+    }
 }
