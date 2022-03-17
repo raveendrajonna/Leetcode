fa40ec6774f40645da9b957294c2ebd67330ce5d
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-17 07:35:44 +0530

    Time: 853 ms (7.90%), Space: 146.8 MB (29.17%) - LeetHub

--- 818-race-car/818-race-car.java
+++ 818-race-car/818-race-car.java
@@ -1,44 +1,45 @@
 class Solution {
     public int racecar(int target) {
-        Queue<int[]> q = new LinkedList<>();
         HashSet<String> visited = new HashSet<>();
-        q.add(new int[]{0,1});
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { 0, 1});
         visited.add("0-1");
-        int dis =0;
+        int distance=0;
+        
         while(q.size() > 0)
         {
             int size = q.size();
-            while(size >0)
+            while(size > 0)
             {
                 size--;
                 int[] cur = q.remove();
-                if(cur[0] == target)
-                    return dis;
+                
+                if(target == cur[0])
+                    return distance;
                 
                 //A
-                int pos = cur[0];
-                int speed = cur[1];
+                int newPos = cur[0]+cur[1]; //curpos+cur speed
+                int newSpeed = 2 * cur[1];
                 
-                int newPos = pos+speed;
-                int newSpeed = 2*speed;
-                if( !visited.contains(newPos+"-"+newSpeed) && Math.abs(target-newPos) < target) 
+                if( !visited.contains(newPos+"-"+newSpeed) & Math.abs(newPos-target) < target)
                 {
+                    q.add(new int[] {newPos, newSpeed} );
                     visited.add(newPos+"-"+newSpeed);
-                    q.add(new int[] { newPos, newSpeed});
-                }
+                } 
                 
-                //if R
-                 newPos = pos;
-                 newSpeed = speed > 0 ? -1: 1;
-                if(!visited.contains(newPos+"-"+newSpeed) && Math.abs(target-newPos) < target)
+                //R
+                newPos = cur[0];
+                newSpeed = cur[1]>0?-1:1;
+                
+               if( !visited.contains(newPos+"-"+newSpeed) & Math.abs(newPos-target) < target)
                 {
-                     visited.add(newPos+"-"+newSpeed);
-                     q.add(new int[]{ newPos, newSpeed});
-                }
+                     q.add(new int[] {newPos, newSpeed} );
+                    visited.add(newPos+"-"+newSpeed);
+                } 
             }
-            dis++;
+            distance++;
         }
-        return dis;
         
+        return -1;
     }
 }
