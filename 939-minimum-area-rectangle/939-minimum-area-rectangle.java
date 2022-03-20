347f39623e8eafbd6172e05b663a8f1da08f5111
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-20 23:01:29 +0530

    Time: 1814 ms (5.02%), Space: 117.9 MB (37.15%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.java
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.java
@@ -1,28 +1,31 @@
 class Solution {
     public int minAreaRect(int[][] points) {
         HashSet<String> hs = new HashSet<>();
-        int res = Integer.MAX_VALUE;
-        for(int i=0; i< points.length; i++)
+        for(int i=0; i<points.length; i++)
         {
-            String key = points[i][0]+"-"+points[i][1];
-            hs.add(key);
+            hs.add(points[i][0]+"@"+points[i][1]);
         }
         
-        for(int i=0; i < points.length; i++)
+        int res = Integer.MAX_VALUE;
+        for(int i=0; i<points.length; i++)
         {
-            for(int j=i+1; j< points.length; j++)
+            for(int j= i+1; j<points.length; j++)
             {
                 int x1 = points[i][0];
                 int y1 = points[i][1];
                 int x2 = points[j][0];
                 int y2 = points[j][1];
                 
-                if(x1 == x2 || y2 == y1)
-                    continue;
-                
-                if(hs.contains(x1+"-"+y2) && hs.contains(x2+"-"+y1))
+                if(x1==x2 || y1== y2)
                 {
-                   res = Math.min(res, Math.abs(x2-x1) * Math.abs(y2-y1));
+                    continue;
+                }
+                
+                if( hs.contains(x1+"@"+y2) && hs.contains(x2+"@"+y1))
+                {
+                    //diagonals found;
+                    int area = (Math.abs(x2-x1) * Math.abs(y2-y1));
+                    res = Math.min(res, area);
                 }
             }
         }
