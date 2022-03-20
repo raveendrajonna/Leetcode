ffb99df458188ad689c3396a559624c063fc5088
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-20 23:25:21 +0530

    Time: 10 ms (18.17%), Space: 45.7 MB (5.43%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -1,25 +1,44 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        HashMap<Integer,String> hms = new HashMap<>();
-        HashMap<Integer, String> hmt = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->(y-x));
-        
-        for(int i=0; i<sources.length; i++)
+        int n = indices.length;
+        Node[] nodes = new Node[n];
+        for(int i=0; i<n; i++)
         {
-           pq.add(indices[i]);
-           hms.put(indices[i], sources[i]);
-           hmt.put(indices[i], targets[i]);
+           Node nd = new Node(indices[i], sources[i], targets[i]);
+            nodes[i] = nd;
         }
+        Arrays.sort(nodes);
         
-        while(pq.size() > 0)
+        for(int i=0; i<n; i++)
         {
-            int cur = pq.remove();
-            String sub = s.substring(cur);
-            if(sub.startsWith(hms.get(cur)))
+            Node nd = nodes[i];
+            int len = nd.source.length();
+           // if(s.substring(nd.idx, nd.idx+len+1).equals(nd.source))
+            if(s.substring(nd.idx).startsWith(nd.source))
             {
-                s = s.substring(0,cur)+hmt.get(cur)+s.substring(cur+hms.get(cur).length());
+                s = s.substring(0,nd.idx)+ nd.dest + s.substring(nd.idx+len);
             }
         }
         return s;
+    }
+    
+    class Node implements Comparable<Node>
+    {
+        int idx;
+        String source;
+        String dest;
+        
+        public Node(int idx, String src, String dest)
+        {
+            this.idx = idx;
+            this.source = src;
+            this.dest = dest;
+        }
+        
+        @Override
+        public int compareTo(Node other)
+        {
+            return other.idx - this.idx;
+        }
     }
 }
