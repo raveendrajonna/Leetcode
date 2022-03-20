47361be0f5e57374289167c09dc46d3dc4400344
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-20 16:19:44 +0530

    Time: 2 ms (24.42%), Space: 43.4 MB (60.85%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,48 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-       // identify index i , where i is grether than i-1 from right
-        //swap idx with the smallest number from right.
-        //sort the numbers from idx
-        //1233333
-       int idx = nums.length-1;
-       while(idx >0 && nums[idx-1] >= nums[idx])
-       {
-           idx--;
-       }
-       // System.out.println(" idx : "+ idx);
-       if(idx > 0)
-       {
-           int k= nums.length-1;
-           while(nums[k] <= nums[idx-1])
-           {
-               k--;
-           }
-           swap(nums,idx-1,k);
-           Arrays.sort(nums,idx,nums.length);
-       }
-        else
+        int n = nums.length;
+        int idx = n-1;
+        while(idx>0 && nums[idx-1] >= nums[idx])
         {
-           reverse(nums);
+            idx--;
         }
-   
+        if(idx == 0)
+        {
+            reverse(nums);
+            return;
+        }
+        
+        int k = n-1;
+        while(idx < k && nums[k] <= nums[idx-1])
+        {
+            k--;
+        }
+        swap(nums, idx-1, k);
+        Arrays.sort(nums,idx,n);
+        
+        
     }
-
+    
     public void reverse(int[] nums)
     {
-        int l =0; int r = nums.length-1;
+        int l =0, r = nums.length-1;
         while(l < r)
         {
-            int temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
+            swap(nums,l, r);
             l++;
             r--;
         }
     }
     
-    public void swap(int[] nums,int i, int j)
+    public void swap(int[] nums, int i , int j)
     {
-        int temp = nums[i];
+        int temp =nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
