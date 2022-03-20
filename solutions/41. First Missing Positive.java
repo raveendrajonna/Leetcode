debdd4aa83fb9d88070da1ccce1314c55c3caff6
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-20 09:14:39 +0530

    1 ms

---
+++ solutions/41. First Missing Positive.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        while(i<n)
+        {
+              if(nums[i] >0 && nums[i] <= n && nums[nums[i]-1] !=  nums[i] )
+              {
+                 swap(nums, nums[i]-1, i);
+              }
+              else
+               {
+                    i++;
+               }
+        }
+        //System.out.println(" sorted : " + Arrays.toString(nums));
+        i = 0;
+        while(i<n && nums[i] == i+1)
+        {
+            i++;
+        }
+        return i+1;
+        
+    }
+    
+    public void swap(int[] nums, int i , int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
