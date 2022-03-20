d7014f00ca48457513ea9a5bab1c2fc34a05d459
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-20 23:26:55 +0530

    14 ms

---
+++ solutions/833. Find And Replace in String.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        int n = indices.length;
+        Node[] nodes = new Node[n];
+        for(int i=0; i<n; i++)
+        {
+           Node nd = new Node(indices[i], sources[i], targets[i]);
+            nodes[i] = nd;
+        }
+        Arrays.sort(nodes, (x,y)-> (y.idx - x.idx));
+        
+        for(int i=0; i<n; i++)
+        {
+            Node nd = nodes[i];
+            int len = nd.source.length();
+           // if(s.substring(nd.idx, nd.idx+len+1).equals(nd.source))
+            if(s.substring(nd.idx).startsWith(nd.source))
+            {
+                s = s.substring(0,nd.idx)+ nd.dest + s.substring(nd.idx+len);
+            }
+        }
+        return s;
+    }
+    
+    class Node
+    {
+        int idx;
+        String source;
+        String dest;
+        
+        public Node(int idx, String src, String dest)
+        {
+            this.idx = idx;
+            this.source = src;
+            this.dest = dest;
+        }
+        
+       /* @Override
+        public int compareTo(Node other)
+        {
+            return other.idx - this.idx;
+        } */
+    }
+}
