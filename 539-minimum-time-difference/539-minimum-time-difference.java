abd709bc97778eb1b74be37e5615683dbe6723ce
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-21 12:29:11 +0530

    Time: 10 ms (58.26%), Space: 43.5 MB (83.64%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.java
+++ 539-minimum-time-difference/539-minimum-time-difference.java
@@ -1,26 +1,29 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-       PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int min = Integer.MAX_VALUE;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = timePoints.size();
+        
         int max = Integer.MIN_VALUE;
-       for(int i=0; i< timePoints.size(); i++)
-       {
-           String[] str = timePoints.get(i).split(":");
-           int h = Integer.parseInt(str[0]);
-           int m = Integer.parseInt(str[1]);
-           int total = h*60+m;
-           max = Math.max(max, total);
-           min = Math.min(min, total);
-           pq.add(total);
-       }
-        int res = Integer.MAX_VALUE;
-        res = (1440-max)+min;
-        int prev = pq.remove(); //removed last one 
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++)
+        {
+            String time = timePoints.get(i);
+            String[] str = time.split(":");
+            int h = Integer.parseInt(str[0]);
+            int m = Integer.parseInt(str[1]);
+            int total = h*60 + m;
+            pq.add(total);
+            max = Math.max(max,total);
+            min = Math.min(min, total);
+        }
+        
+        int res = (1440-max+min);
+        int prev = pq.remove();
         while(pq.size() > 0)
         {
-           int cur = pq.remove();
-           res = Math.min(res, cur-prev);
-           prev = cur;
+            int cur = pq.remove();
+            res = Math.min(cur-prev, res);
+            prev = cur;
         }
         return res;
     }
