eac8109bbf9a6fb454ed1bac8a9f8dbc38ee20bc
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-21 12:16:23 +0530

    Time: 7 ms (7.59%), Space: 67.7 MB (5.36%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,21 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length;
-        int[] l = new int[n];
-        int[] r= new int[n];
-        l[0] = cardPoints[0];
-        r[n-1] = cardPoints[n-1];
-        for(int i=1; i<k; i++)
+       int n = cardPoints.length;
+        int[] la = new int[n];
+        int[] ra = new int[n];
+        la[0] = cardPoints[0];
+        ra[n-1] = cardPoints[n-1];
+        for(int i=1; i<n; i++)
         {
-            l[i] = l[i-1]+cardPoints[i];
-            r[n-1-i] = r[n-i] + cardPoints[n-1-i];
+            la[i] = cardPoints[i] + la[i-1];
+            ra[n-1-i] = cardPoints[n-1-i]+ ra[n-i];
         }
         
-        int res = Math.max(l[k-1] , r[n-k]);
-        for(int i=1; i< k ; i++)
+        int res = Math.max(la[k-1], ra[n-k]);
+        for(int i=1; i<k; i++)
         {
-            int rcnt = k-i;
-            int cur =  l[i-1] + r[n-rcnt];
-            res = Math.max(res, cur);
+            res = Math.max(res,  la[i-1] + ra[n-k+i] );
         }
         return res;
-  }
+    }
 }
