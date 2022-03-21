b650b655bc2771940b24c66cebc9338070284f77
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-21 16:20:43 +0530

    Time: 201 ms (48.82%), Space: 160.3 MB (14.66%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,37 +1,38 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-        int[][] t = new int[tasks.length][3];
+        int[][] dp = new int[tasks.length][3];
         int n = tasks.length;
-        for(int i=0; i<n; i++)
+        for(int i=0; i<tasks.length; i++)
         {
-            t[i] = new int[]{ tasks[i][0], tasks[i][1], i}; 
+            dp[i][0] =tasks[i][0];
+            dp[i][1] = tasks[i][1];
+            dp[i][2] = i;
         }
         
-        Arrays.sort(t, (x,y)->(x[0]-y[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)->( x[0]==y[0]?x[1]-y[1]:x[0]-y[0]));
-        
-        int i=0, j=0;
+        Arrays.sort(dp, (x,y)->(x[0]==y[0]?x[1]-y[1]:x[0]-y[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (x,y)-> (x[0]==y[0]? x[1]-y[1] : x[0]-y[0]));
+        int i=0;
+        int time =0;
         int[] res = new int[n];
-        int time = 0;
-        
-        while( i < t.length)
+        int idx=0;
+        while(i<n)
         {
-           while(i<n && time >= t[i][0])   
-           {
-               int[] cur = t[i++];
-               pq.add(new int[] { cur[1],cur[2]});
-           }
+            while(i<n && time >= dp[i][0])
+            {
+                pq.add(new int[] {  dp[i][1] , dp[i][2] });
+                i++;
+            }
             if(pq.size() > 0)
             {
                 int[] cur = pq.remove();
-                res[j++] = cur[1];
+                res[idx++] = cur[1];
                 time = time+cur[0];
             }
             else
             {
                 if(i<n)
                 {
-                    time = t[i][0];
+                    time = dp[i][0];
                 }
             }
         }
@@ -39,9 +40,8 @@ class Solution {
         while(pq.size() > 0)
         {
             int[] cur = pq.remove();
-            res[j++] = cur[1];
-            
+            res[idx++] = cur[1];
         }
-        return res;                                  
+        return res;
     }
 }
