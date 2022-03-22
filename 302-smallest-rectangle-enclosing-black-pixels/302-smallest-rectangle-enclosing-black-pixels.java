8934e3669cab43f0abc803d5d195730837a23431
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-22 23:19:10 +0530

    Time: 5 ms (28.53%), Space: 56.1 MB (52.00%) - LeetHub

--- 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.java
+++ 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.java
@@ -1,34 +1,37 @@
 class Solution {
-    int[][] dir = new int[][] { {-1,0} , {0, 1} , {1,0} , {0,-1} };
-    int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE, minY= Integer.MAX_VALUE, maxY = Integer.MIN_VALUE;
+    int[][] dir = new int[][] { {-1,0} , {0,1} , {1,0} , {0, -1}};
+    int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+    int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+    
     public int minArea(char[][] image, int x, int y) {
         boolean[][] visited = new boolean[image.length][image[0].length];
-        util(image, x, y , visited);
-        return (maxX-minX+1) * (maxY - minY+1);
+        util(image,x,y,visited);
+        return (maxX-minX+1)* (maxY-minY+1);
     }
     
-    public void util(char[][] image, int x, int y,boolean[][] visited)
+    public void util(char[][] image, int x, int y, boolean[][] visited)
     {
         if(visited[x][y])
-            return ;
+            return;
         
         visited[x][y] = true;
         
-        minX = Math.min(x, minX);
-        maxX = Math.max(x, maxX);
-        minY = Math.min(y, minY);
-        maxY = Math.max(y, maxY);
-       // System.out.println(" x  : " + x +", y : " + y);
-        for(int i=0; i<4; i++)
+        minX = Math.min(minX, x);
+        maxX = Math.max(maxX, x);
+        minY = Math.min(minY, y);
+        maxY = Math.max(maxY, y);
+        
+        for(int i=0; i< 4; i++)
         {
             int nr = x + dir[i][0];
             int nc = y + dir[i][1];
             
-            if(nr >= 0 && nr < image.length && nc >=0 && nc < image[0].length && !visited[nr][nc] && image[nr][nc] == '1')
+            if(nr>= 0 && nr< image.length && nc>=0 && nc< image[0].length && image[nr][nc] == '1' && !visited[nr][nc])
             {
-                util(image, nr,nc, visited);
+                util(image, nr, nc, visited);
             }
         }
+        
         
     }
 }
