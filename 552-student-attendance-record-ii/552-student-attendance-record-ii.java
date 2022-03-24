c86544d121365440c2a750163a1b3db02c7c4767
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-24 10:12:17 +0530

    Time: 649 ms (22.03%), Space: 143.6 MB (17.12%) - LeetHub

--- 552-student-attendance-record-ii/552-student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.java
@@ -1,32 +1,26 @@
 class Solution {
     public int checkRecord(int n) {
        int[][][] mem = new int[n][2][3];
-       return util(0,n,0,0,mem);
+        return util(0, n, 0, 0, mem);
     }
     
-    public int util(int i, int n, int a, int l,int[][][] mem)
+    public int util(int i, int n, int a, int l, int[][][] mem)
     {
-        if(i == n) 
-           return 1;
+        if(i == n)
+            return 1;
         
-        if(mem[i][a][l] != 0)
+        if(mem[i][a][l] != 0) 
             return mem[i][a][l];
         
-        //p
-        long res =0;
-        res = res + util(i+1,n, a, 0, mem);
+        long res = util(i+1, n, a, 0, mem);
         
-        //a
         if(a == 0)
-            res = res+ util(i+1, n, a+1, 0, mem);
+            res += util(i+1, n, a+1, 0, mem);
         
-        if(l <2)
-        {
-            res =  res + util(i+1, n, a, l+1, mem);
-        }
-       
-        mem[i][a][l] = (int)(res%1000000007);
+        if(l<2)
+           res += util(i+1, n, a, l+1, mem);
         
+        mem[i][a][l] = (int) (res%1000000007);
         return mem[i][a][l];
     }
 }
