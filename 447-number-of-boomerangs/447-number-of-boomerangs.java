0c43ab26a2dd2441ea84475c8798bd834be44d4e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-24 17:28:10 +0530

    Time: 192 ms (61.49%), Space: 112.7 MB (58.96%) - LeetHub

--- 447-number-of-boomerangs/447-number-of-boomerangs.java
+++ 447-number-of-boomerangs/447-number-of-boomerangs.java
@@ -1,57 +1,34 @@
 class Solution {
     public int numberOfBoomerangs(int[][] points) {
-       /* HashMap<Integer,Integer> hm = new HashMap<>();
+       HashMap<Integer,Integer> hm = new HashMap<>();
+        int res=0;
+        
         for(int i=0; i<points.length; i++)
         {
             for(int j=0; j<points.length; j++)
             {
-                if(i == j)
+                if(i== j)
                     continue;
-
-                int d = getDistance(points[i],points[j]);       
+                
+                int d = getDistinace(points[i], points[j]);
                 hm.put(d, hm.getOrDefault(d,0)+1);
             }
-        }
-        int res =0;
-        for(Map.Entry<Integer,Integer> entry : hm.entrySet())
-        {
-            System.out.println("val : " + entry.getValue() +", key : " + entry.getKey());
-            int val = entry.getValue();
-            res = res + (val * (val-1));
-        }
-         return res;
-        */
-        
-         int res = 0;
-
-    Map<Integer, Integer> map = new HashMap<>();
-    for(int i=0; i<points.length; i++) {
-        for(int j=0; j<points.length; j++) {
-            if(i == j)
-                continue;
             
-            int d = getDistance(points[i], points[j]);                
-            map.put(d, map.getOrDefault(d, 0) + 1);
+            for(int val : hm.values())
+            {
+                //System.out.println("i :"+i +", val :"+ val);
+                res = res + (val * (val-1));
+            }
+           // System.out.println("*****");
+            hm.clear();
         }
-        
-      for(Map.Entry<Integer,Integer> entry : map.entrySet())
-        {
-            int val = entry.getValue();
-            res = res + (val * (val-1));
-        }
-        map.clear(); 
+        return res;
     }
     
-    return res;
-        
-        
-       
+    public int getDistinace(int[] p1, int[] p2)
+    {
+        int dx = p1[0] - p2[0];
+        int dy = p1[1] - p2[1];
+        return (dx*dx)+(dy*dy);
     }
-    
-    private int getDistance(int[] a, int[] b) {
-    int dx = a[0] - b[0];
-    int dy = a[1] - b[1];
-    
-    return dx*dx + dy*dy;
-}
 }
