ff10fdbece8198003c58a49a68adf35015649207
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-24 23:03:46 +0530

    Time: 74 ms (48.94%), Space: 67.4 MB (77.98%) - LeetHub

--- 715-range-module/715-range-module.java
+++ 715-range-module/715-range-module.java
@@ -1,60 +1,51 @@
 class RangeModule {
 
-    public TreeMap<Integer,Integer> tm ;
+    TreeMap<Integer,Integer> tm ;
     public RangeModule() {
-        this.tm = new TreeMap<>();
+        tm = new TreeMap<>();
     }
     
     public void addRange(int left, int right) {
-        Integer start= tm.floorKey(left);
+        Integer start = tm.floorKey(left);
         Integer end = tm.floorKey(right);
-        // System.out.println(" before add tm :" + tm +", left : " + left +", right : " + right + " end :" + end);
+        
         if(start != null && tm.get(start) >= left)
         {
             left = start;
         }
         
-        if(end != null && tm.get(end) > right)
+        if(end != null && tm.get(end) >= right)
         {
             right = tm.get(end);
         }
         
         tm.put(left, right);
-        
-        
-        tm.subMap(left, false, right, true).clear();
-        //System.out.println(" after add tm :" + tm +", left : " + left +", right : " + right);
+        tm.subMap(left,false, right, true).clear();
     }
     
     public boolean queryRange(int left, int right) {
+       Integer start = tm.floorKey(left);
+       if(start == null)
+           return false;
         
-        Integer start = tm.floorKey(left);
-       // System.out.println(" tm : " + tm +", left : " + left +", right : " + right);
-        if (start == null) return false;
-        return tm.get(start) >= right;
-        
+        return tm.get(start)>= right;
     }
     
     public void removeRange(int left, int right) {
-      // System.out.println(" tm :" + tm);
-       Integer start = tm.floorKey(left);
-       Integer end = tm.floorKey(right);
+        Integer start = tm.floorKey(left);
+        Integer end = tm.floorKey(right);
         
-
-        if(end != null && tm.get(end) > end)
+        if(end != null && tm.get(end) >= end)
         {
             tm.put(right, tm.get(end));
         }
         
-        if(start != null && tm.get(start) > left)
+        if(start != null &&  tm.get(start)>= left)
         {
             tm.put(start, left);
         }
         
-
-        
-        tm.subMap(left, true, right, false).clear();   
-
+        tm.subMap(left, true, right, false).clear();
     }
 }
 
