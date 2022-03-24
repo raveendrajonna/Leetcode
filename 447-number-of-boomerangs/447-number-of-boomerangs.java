86df16f69dc2e4051fde560acad8ce79870e9feb
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-24 17:44:37 +0530

    Time: 253 ms (38.38%), Space: 107.2 MB (78.35%) - LeetHub

--- 447-number-of-boomerangs/447-number-of-boomerangs.java
+++ 447-number-of-boomerangs/447-number-of-boomerangs.java
@@ -1,34 +1,31 @@
 class Solution {
     public int numberOfBoomerangs(int[][] points) {
-       HashMap<Integer,Integer> hm = new HashMap<>();
+      int n = points.length;
+        HashMap<Integer,Integer> hm = new HashMap<>();
         int res=0;
-        
-        for(int i=0; i<points.length; i++)
+        for(int i=0; i<n; i++)
         {
-            for(int j=0; j<points.length; j++)
+            for(int j=0; j<n; j++)
             {
-                if(i== j)
+                if(i == j)
                     continue;
                 
-                int d = getDistinace(points[i], points[j]);
+                int d = getDistance(points[i], points[j]);
                 hm.put(d, hm.getOrDefault(d,0)+1);
             }
-            
             for(int val : hm.values())
             {
-                //System.out.println("i :"+i +", val :"+ val);
                 res = res + (val * (val-1));
             }
-           // System.out.println("*****");
             hm.clear();
         }
         return res;
     }
     
-    public int getDistinace(int[] p1, int[] p2)
+    public int getDistance(int[] p1, int[] p2)
     {
         int dx = p1[0] - p2[0];
         int dy = p1[1] - p2[1];
-        return (dx*dx)+(dy*dy);
+        return (dx*dx)+ (dy*dy);
     }
 }
