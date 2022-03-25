9ede478432f05f8d44042b722c40b62bfd4dbedd
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-25 23:25:06 +0530

    Time: 124 ms (30.42%), Space: 55.5 MB (54.00%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.java
+++ 975-odd-even-jump/975-odd-even-jump.java
@@ -1,30 +1,31 @@
 class Solution {
     public int oddEvenJumps(int[] arr) {
-        int n = arr.length;
-        boolean[] low = new boolean[n];
-        boolean[] high = new boolean[n];
         TreeMap<Integer,Integer> tm = new TreeMap<>();
+        int n = arr.length;
+        boolean[] high = new boolean[n];
+        boolean[] low = new boolean[n];
         
-        low[n-1] = true;
         high[n-1] = true;
-        tm.put(arr[n-1], n-1);
+        low[n-1] = true;
         int res =1;
-        for(int i= n-2; i>=0 ; i--)
+        tm.put(arr[n-1], n-1);
+        for(int i=n-2; i>= 0; i--)
         {
-           Integer h = tm.ceilingKey(arr[i]);
-           Integer l = tm.floorKey(arr[i]);
+            Integer h = tm.ceilingKey(arr[i]);
+            Integer l  = tm.floorKey(arr[i]);
             
-         if(h != null && low[tm.get(h)])
-            high[i] = true;
-         
-         if(l != null && high[tm.get(l)])
-             low[i] = true;
-        
-         if(high[i])
-             res++;
+            if(h != null && low[tm.get(h)])
+                high[i] = true;
             
-          tm.put(arr[i], i);
+            if(l != null && high[tm.get(l)])
+                low[i] = true;
+            
+            if(high[i])
+                res++;
+            
+            tm.put(arr[i], i);
         }
+        
         return res;
     }
 }
