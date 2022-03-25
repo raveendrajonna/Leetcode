8246e691b9fc9f93aaa5b4620430a4a0cb90eb0a
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-25 09:11:56 +0530

    Time: 264 ms (23.56%), Space: 163.5 MB (21.79%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,93 +1,94 @@
 class Solution {
-    TreeMap<Integer,HashSet<Integer>> tm;
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        tm = new TreeMap<>();
         UF uf = new UF(n);
-        uf.union(0,firstPerson);
-        
+        uf.union(0, firstPerson);
+        TreeMap<Integer,HashSet<Integer>> tm = new TreeMap<>();
         for(int i=0; i<meetings.length; i++)
         {
-            tm.putIfAbsent(meetings[i][2],new HashSet<>());
+            tm.putIfAbsent(meetings[i][2], new HashSet<>());
             tm.get(meetings[i][2]).add(i);
         }
         
-        for( int key : tm.keySet())
+        for(int key : tm.keySet())
         {
             HashSet<Integer> pool = new HashSet<>();
             for(int cur : tm.get(key))
             {
                 int x = meetings[cur][0];
                 int y = meetings[cur][1];
-                uf.union(x,y);
                 pool.add(x);
-                pool.add(y);
+                pool.add(y); 
+                uf.union(x,y);
             }
-            
-            for(int x : pool)
+        
+            for(int cur : pool)
             {
-               // System.out.println( "x :" + x +", parent  : " + uf.find(x));
-                if(!uf.isConnected(0, x))
+                if(!uf.isConnected(0, cur))
                 {
-                   uf.parent[x] = x;  
+                    uf.parent[cur] = cur;
                 }
             }
         }
         
         List<Integer> res = new ArrayList<>();
-        for(int i=0 ; i<n; i++)
+        for(int i=0; i<n; i++)
         {
-            //System.out.println( "x :" + i +", parent  : " + uf.find(i));
-            if(uf.find(i)== 0)
+            if(uf.find(i) == 0)
             {
                 res.add(i);
             }
         }
+        
         return res;
     }
     
-    public class UF
-    {
-        int[] parent;
-        
-        public UF(int n)
-        {
+   class UF
+   {
+       int[] parent ;
+       
+       public UF(int n)
+       {
            parent = new int[n];
-            for(int i=0; i<n; i++)
-            {
-                parent[i] = i;
-            }
-        }
-        
-        public boolean isConnected(int x, int y)
-        {
-            int px = find(x);
-            int py = find(y);
-            return px == py;
-        }
-        
-        public void union(int x, int y)
-        {
-            int px = find(x);
-            int py = find(y);
-            if(px == py)
-                return;
-            
-            if(px < py)
-            {
-                parent[py] = px;
-            }
-            else
-            {
-                parent[px] = py;
-            }                 
-        }
-        
-        public int find(int x)
-        {
-            if(x != parent[x])
-                return find(parent[x]);
-            
-            return x;
-        }        
-    }
+           
+           for(int i=0; i<n; i++)
+           {
+               parent[i] = i;
+           }
+       }
+       
+       public int find(int x)
+       {
+           if(x != parent[x])
+              parent[x] = find(parent[x]);
+           
+           return parent[x];
+       }
+       
+       public void union(int x, int y)
+       {
+          int px = find(x);
+          int py = find(y);
+           
+           if(px == py)
+               return;
+           
+           if(px < py)
+               parent[py] = px;
+           else
+               parent[px] = py;
+       }
+       
+       public boolean isConnected(int x, int y)
+       {
+           int px = find(x);
+           int py = find(y);
+           
+           return px == py;
+       }
+       
+       public void reset(int x)
+       {
+           parent[x] = x;
+       }
+   }
 }
