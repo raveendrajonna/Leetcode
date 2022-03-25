81615b8710474e9967a9b45db343f38f4ed016ee
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-25 23:30:35 +0530

    Time: 104 ms (49.20%), Space: 55.7 MB (53.04%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.java
+++ 975-odd-even-jump/975-odd-even-jump.java
@@ -1,18 +1,18 @@
 class Solution {
     public int oddEvenJumps(int[] arr) {
-        TreeMap<Integer,Integer> tm = new TreeMap<>();
-        int n = arr.length;
+       TreeMap<Integer, Integer> tm = new TreeMap<>();
+       int n = arr.length;
         boolean[] high = new boolean[n];
         boolean[] low = new boolean[n];
-        
         high[n-1] = true;
         low[n-1] = true;
-        int res =1;
+        
         tm.put(arr[n-1], n-1);
-        for(int i=n-2; i>= 0; i--)
+        int res =0;
+        for(int i=n-1; i>=0; i--)
         {
             Integer h = tm.ceilingKey(arr[i]);
-            Integer l  = tm.floorKey(arr[i]);
+            Integer l = tm.floorKey(arr[i]);
             
             if(h != null && low[tm.get(h)])
                 high[i] = true;
@@ -22,10 +22,9 @@ class Solution {
             
             if(high[i])
                 res++;
-            
             tm.put(arr[i], i);
         }
-        
         return res;
+        
     }
 }
