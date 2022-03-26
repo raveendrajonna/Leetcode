25244b364037b194cf4f3dda1617b7c44856224b
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-26 12:07:07 +0530

    Time: 7 ms (30.14%), Space: 41.5 MB (31.51%) - LeetHub

--- 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.java
+++ 1240-tiling-a-rectangle-with-the-fewest-squares/1240-tiling-a-rectangle-with-the-fewest-squares.java
@@ -1,69 +1,67 @@
 class Solution {
     int ans = Integer.MAX_VALUE;
-    int n, m;
+    int n,m;
     public int tilingRectangle(int n, int m) {
         this.n = n;
         this.m = m;
-        dfs(0,0, new boolean[n][m], 0);
+        dfs(0, 0, new boolean[n][m], 0);
         return ans;
     }
- 
     
     public void dfs(int r, int c, boolean[][] visited, int cnt)
     {
-        if(cnt >= ans)
-        {
+        if(cnt > ans)
             return;
-        }
         
         if(r >= n)
         {
-            ans =cnt;
+            ans = cnt;
             return;
         }
         
-        if(c >= m)
+        if( c >= m)
         {
             dfs(r+1, 0, visited, cnt);
             return;
         }
         
-        
         if(visited[r][c])
         {
-            dfs(r, c+1, visited, cnt);
+            dfs(r,c+1, visited, cnt);
             return;
         }
         
-
+        for(int size = Math.min(n-r, m-c); size > 0 && isAvailable(r,c, size, visited); size--)
+        {
+            coverOrUncover(r,c, visited, size, true);
+            dfs(r,c+size, visited, cnt+1);
+            coverOrUncover(r,c, visited, size , false);
+            
+        }
         
-        for(int k= Math.min(n-r, m-c); k>=1 && isAvailable(r, c, visited, k) ; k--)
-        {
-            cover(r, c, visited, k, true);
-            dfs(r, c+k, visited, cnt+1);
-            cover(r,c, visited, k, false);
-        }
     }
     
-    public void cover(int r, int c, boolean[][] visited, int k, boolean value)
+    public void coverOrUncover(int r, int c, boolean[][] visited, int size, boolean val)
     {
-        for(int i=r; i<r+k; i++)
+        for(int i=r; i< r+size; i++)
         {
-            for(int j=c; j< c+k; j++)
+            for(int j=c; j< c+ size; j++)
             {
-               visited[i][j] = value;
+                visited[i][j] = val;
             }
         }
     }
     
-    public boolean isAvailable(int r, int c, boolean[][] visited, int k)
+    public boolean isAvailable(int r, int c, int size, boolean[][] visited)
     {
-        for (int i = r; i < r+k; i++){
-            for (int j = c; j < c+k; j++){
-                if (visited[i][j]) return false;
-            }
-        }
+       for(int i=r; i<r+size; i++)
+       {
+           for(int j= c; j< c+size; j++)
+           {
+               if(visited[i][j]) 
+                   return false;
+           }
+       }
         return true;
     }
-    
 }
