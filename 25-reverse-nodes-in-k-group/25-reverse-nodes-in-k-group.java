fb341cdaf5401b6d3d79b4cf3a19b16fbb0d37cb
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-26 16:36:28 +0530

    Time: 5 ms (6.33%), Space: 45.2 MB (71.38%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -12,35 +12,36 @@ class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode dummy = new ListNode(-1);
         ListNode prev = dummy;
+        
         ListNode cur = head;
         Stack<ListNode> stk = new Stack<>();
         while(cur != null)
         {
-            int cnt=0;
+            int cnt =0;
             while(cnt <k && cur != null)
             {
-              cnt++;
-              stk.push(cur);
-              cur = cur.next;
+                stk.push(cur);
+                cur = cur.next;
+                cnt++;
             }
-            
             if(cnt == k)
             {
-               while(stk.size() > 0)
-               {
-                  prev.next = stk.pop();
-                  prev = prev.next;
-               }
+                while(stk.size() > 0)
+                {
+                    prev.next = stk.pop();
+                    prev = prev.next;
+                }
                 if(cur == null)
                     prev.next = null;
             }
             else
             {
                 while(stk.size() > 1)
+                {
                     stk.pop();
-                
+                }
                 prev.next = stk.pop();
-            }
+            } 
         }
         return dummy.next;
     }
