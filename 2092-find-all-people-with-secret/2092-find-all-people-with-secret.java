826d5cd956b2c2894d8cca79d945b9922bb76f4d
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-26 12:41:40 +0530

    Time: 325 ms (15.57%), Space: 163.3 MB (22.29%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -2,6 +2,7 @@ class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
         UF uf = new UF(n);
         uf.union(0, firstPerson);
+        
         TreeMap<Integer,HashSet<Integer>> tm = new TreeMap<>();
         for(int i=0; i<meetings.length; i++)
         {
@@ -9,23 +10,24 @@ class Solution {
             tm.get(meetings[i][2]).add(i);
         }
         
-        for(int key : tm.keySet())
+        for(int time : tm.keySet())
         {
-            HashSet<Integer> pool = new HashSet<>();
-            for(int cur : tm.get(key))
+           HashSet<Integer> hs = new HashSet<>();
+            for(int idx : tm.get(time))
             {
-                int x = meetings[cur][0];
-                int y = meetings[cur][1];
-                pool.add(x);
-                pool.add(y); 
+                int x = meetings[idx][0];
+                int y = meetings[idx][1];
+                
+                hs.add(x);
+                hs.add(y);
                 uf.union(x,y);
             }
-        
-            for(int cur : pool)
+            
+            for(int x : hs)
             {
-                if(!uf.isConnected(0, cur))
+                if(!uf.isConnected(0, x))
                 {
-                    uf.parent[cur] = cur;
+                    uf.reset(x);
                 }
             }
         }
@@ -33,62 +35,57 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         for(int i=0; i<n; i++)
         {
-            if(uf.find(i) == 0)
+            if(uf.isConnected(0, i))
             {
                 res.add(i);
             }
         }
-        
         return res;
     }
     
-   class UF
-   {
-       int[] parent ;
-       
-       public UF(int n)
-       {
-           parent = new int[n];
-           
-           for(int i=0; i<n; i++)
-           {
-               parent[i] = i;
-           }
-       }
-       
-       public int find(int x)
-       {
-           if(x != parent[x])
-              parent[x] = find(parent[x]);
-           
-           return parent[x];
-       }
-       
-       public void union(int x, int y)
-       {
-          int px = find(x);
-          int py = find(y);
-           
-           if(px == py)
-               return;
-           
-           if(px < py)
-               parent[py] = px;
-           else
-               parent[px] = py;
-       }
-       
-       public boolean isConnected(int x, int y)
-       {
-           int px = find(x);
-           int py = find(y);
-           
-           return px == py;
-       }
-       
-       public void reset(int x)
-       {
-           parent[x] = x;
-       }
-   }
+    public class UF
+    {
+        int[] parent ;
+        
+        public UF(int n)
+        {
+            parent = new int[n];
+            for(int i=0; i<n; i++)
+            {
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int x)
+        {
+            if(x != parent[x])
+                parent[x] = find(parent[x]);
+            
+            return parent[x];
+        }
+        
+        public void union(int x, int y)
+        {
+            int px = find(x);
+            int py = find(y);
+            
+            if(px < py)
+                parent[py] = px;
+            else
+                parent[px] = py;
+        }
+        
+        public boolean isConnected(int x, int y)
+        {
+            int px = find(x);
+            int py = find(y);
+            
+            return px == py;
+        }
+        
+        public void reset(int x)
+        {
+            parent[x] = x;
+        }
+    }
 }
