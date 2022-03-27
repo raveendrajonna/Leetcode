f7068a6c06b6aed091bde38e32613c36d131efec
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 21:29:57 +0530

    Time: 323 ms (31.59%), Space: 61.2 MB (86.21%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         int n = changed.length;
-        if(n%2 == 1)
+        if(n%2 != 0)
             return new int[0];
         
-        
-        TreeMap<Integer,Integer> hm = new TreeMap<>();
-        for(int i=0; i<n; i++)
+        TreeMap<Integer,Integer> tm = new TreeMap<>();
+        for(int i=0; i<changed.length; i++)
         {
-            hm.put(changed[i], hm.getOrDefault(changed[i],0)+1);
+            tm.put(changed[i], tm.getOrDefault(changed[i],0)+1);
         }
+        
         int[] res = new int[n/2];
         int idx=0;
         
-        for(int x : hm.keySet())
+        for(int i : tm.keySet())
         {
-            if(hm.getOrDefault(x,0) > hm.getOrDefault(x+x,0))
+            if(tm.get(i) > tm.getOrDefault(i+i,0))
             {
                 return new int[0];
             }
-
-            for(int i=0 ;i<hm.get(x); i++)
+            
+            for(int k=0; k<tm.get(i) ; k++)
             {
-
-                res[idx++] = x;
-                hm.put(x+x, hm.get(x+x)-1);
+                res[idx++] = i;
+                tm.put(i+i, tm.getOrDefault(i+i, 0)-1);
             }
         }
         return res;
