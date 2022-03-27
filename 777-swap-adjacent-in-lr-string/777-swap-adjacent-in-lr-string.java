4f5318766471cd3a7572455083bd7bb44ad33aa3
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-28 01:00:47 +0530

    Time: 5 ms (75.87%), Space: 44 MB (59.37%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
@@ -1,45 +1,41 @@
 class Solution {
     public boolean canTransform(String start, String end) {
-      int i=0;
-      int j=0;
-        
-     int n = start.length();
-        while(i < n && j < n)
-        {
-            while(j<n && end.charAt(j) == 'X')
+       int i=0, j=0; 
+        int n = start.length();
+       while(i <n && j<n)
+       {
+          while(i<n && start.charAt(i) == 'X')
+          {
+              i++;
+          }
+
+           while(j < n && end.charAt(j) == 'X')
+           {
                j++;
-            
-            while(i<n && start.charAt(i) =='X')
-                i++;
-            
-            if(i == n || j ==n)
-                break;
-            
-            if(i<n && j<n && start.charAt(i) == end.charAt(j))
-            {
-                if(end.charAt(j) == 'L' && i < j )
-                    return false;
-                
-                if(end.charAt(j) == 'R' && i > j)
-                    return false;
-                
-                i++;
-                j++;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        while(i<n && start.charAt(i) == 'X')
+           }
+           
+           if(i <n && j < n && start.charAt(i) != end.charAt(j))
+           {
+               return false; 
+           }
+           else if( i <n && j < n && start.charAt(i) =='L' && i < j )
+           {
+               return false;
+           }
+           else if(i<n && j <n && start.charAt(i) =='R' && i > j)
+           {
+               return false;
+           }
+           i++;
+           j++;
+       }
+        
+        while(i <n && start.charAt(i) =='X' )
             i++;
         
-        while(j<n && end.charAt(j) == 'X')
+        while(j <n && end.charAt(j) == 'X')
             j++;
-        
-        System.out.println(" i : " + i +", j : " + j );
-        if(i==n && j == n)
-            return true;
-        return false;
+       // System.out.println(" i :"+ i +", j : "+ j);
+        return i==j;
     }
 }
