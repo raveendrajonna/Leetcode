6f4c56cac064372be2cfb23e6e5f905e4863089e
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 08:11:42 +0530

    Time: 68 ms (17.57%), Space: 49.7 MB (24.30%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-       int m = s.length();
+        int m = s.length();
         int n = p.length();
+        
         boolean[][] dp = new boolean[m+1][n+1];
-        
-       dp[0][0] = true;
-        
+        dp[0][0] = true;
         for(int i=1; i<=n; i++)
         {
             if(p.charAt(i-1) == '*')
-            {
                 dp[0][i] = dp[0][i-1];
-            }
         }
         
         for(int i=1; i<=m; i++)
@@ -20,7 +17,7 @@ class Solution {
             {
                 if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?')
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                  dp[i][j] = dp[i-1][j-1];
                 }
                 else if(p.charAt(j-1) == '*')
                 {
@@ -28,8 +25,6 @@ class Solution {
                 }
             }
         }
-        
         return dp[m][n];
-        
     }
 }
