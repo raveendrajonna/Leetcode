299e2bddd83e39dec7c614a0d5d5e42ccea3a485
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 18:59:48 +0530

    Time: 51 ms (13.24%), Space: 60.9 MB (22.83%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.java
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.java
@@ -15,22 +15,25 @@
  */
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        HashMap<String,Integer> hm = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
         List<TreeNode> res = new ArrayList<>();
-        postOrder(root, hm, res);
+        preOrder(root, hm, res);
         return res;
     }
     
-    public String postOrder(TreeNode root, HashMap<String,Integer> hm, List<TreeNode> res )
+    public String preOrder(TreeNode root, HashMap<String,Integer> hm , List<TreeNode> res)
     {
-        if(root == null) return "#";
+        if(root == null)
+            return "null" ;
         
-        String key = root.val +","+postOrder(root.left, hm, res)+","+postOrder(root.right, hm, res);
-        hm.put(key, hm.getOrDefault(key,0)+1);
-        if(hm.get(key) == 2)
-        {
-            res.add(root);
-        }
-        return key;
+
+        String cur = root.val + "@" +preOrder(root.left, hm, res) +"@" +preOrder(root.right,hm,res);
+        
+         hm.put(cur, hm.getOrDefault(cur,0)+1);
+         if(hm.get(cur) == 2)
+         {
+             res.add(root);
+         } 
+        return cur;
     }
 }
