dc95b791801e6fc092e5d0e3d5f1efa8e8260455
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 14:50:28 +0530

    Time: 16 ms (34.57%), Space: 54 MB (54.68%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,43 +1,44 @@
 class Solution {
-    int[][] dirArr = new int[][] { {-1, 0}, {0,1}, {1,0} , {0, -1}};
-    int res =0;
-    
+    int rows, cols;
+    int ans;
+    int[][] dirArr = new int[][] { {-1,0}, {0,1} , {1,0} , {0, -1}};
     public int longestIncreasingPath(int[][] matrix) {
-        int[][] visited = new int[matrix.length][matrix[0].length];
-       for(int i=0; i<matrix.length; i++)
-       {
-           for(int j=0; j< matrix[0].length; j++)
-           {
-               dfs(matrix, i, j, -1, visited);
-           }
-       }
-      return res;
+        rows = matrix.length;
+        cols = matrix[0].length;
+        int[][] visited = new int[rows][cols];
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                int val = dfs(i,j, matrix, visited, -1);
+                ans = Math.max(ans,val);
+            }
+        }
+        return ans;
+        
     }
     
-    
-    public int dfs(int[][] matrix, int r, int c, int prevValue, int[][] visited)
+    public int dfs(int r, int c, int[][] matrix, int[][] visited, int prevVal)
     {
-       
-        
-        if(r>=0 && r< matrix.length && c>=0 && c<matrix[0].length &&  matrix[r][c] > prevValue)
+        if(r>=0 && r<rows && c >= 0 && c<cols && prevVal < matrix[r][c])
         {
             if(visited[r][c] != 0)
-                  return visited[r][c];
-
+                return visited[r][c];
+            
             for(int i=0; i<4; i++)
             {
                 int nr = r+dirArr[i][0];
                 int nc = c + dirArr[i][1];
-                visited[r][c] = Math.max(visited[r][c], 1 + dfs(matrix, nr, nc, matrix[r][c], visited));
+                
+                visited[r][c] = Math.max(visited[r][c] , 1+ dfs(nr, nc, matrix, visited, matrix[r][c]));
             }
-         
-            res = Math.max(res, visited[r][c]);
+            
             return visited[r][c];
         }
         else
         {
             return 0;
         }
-
     }
+    
 }
