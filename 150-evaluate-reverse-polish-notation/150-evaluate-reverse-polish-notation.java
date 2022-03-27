846069255401ace69c8a896e2402c48a36d91e24
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 15:41:21 +0530

    Time: 13 ms (18.35%), Space: 44 MB (62.74%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,50 +1,35 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int n = tokens.length;
+        int res=0;
         Stack<Integer> stk = new Stack<>();
         for(String token : tokens)
         {
-            if("+".equals(token) ||
-              "-".equals(token) || 
-               "*".equals(token) ||
-               "/".equals(token))
+            if("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token))
             {
-                int n2 = stk.pop();
-                int n1 = stk.pop();
-                stk.push(handle(n1, n2, token.charAt(0)));
+                int num2 = stk.pop();
+                int num1 = stk.pop();
+                if("+".equals(token))
+                {
+                  stk.push(num1 + num2);
+                }
+                else if("-".equals(token))
+                {
+                    stk.push(num1-num2);
+                }
+                else if("*".equals(token))
+                {
+                    stk.push(num1*num2);
+                }
+                else
+                {
+                    stk.push(num1/num2);
+                }
             }
             else
             {
-                 stk.push(Integer.parseInt(token));
+                stk.push( Integer.parseInt(token));
             }
         }
-        
-        int res=0;
-        while(stk.size() > 0)
-        {
-            res = res + stk.pop();
-        }
-        return res;
-    }
-    
-    public int handle(int n1, int n2, char c)
-    {
-        if(c == '+')
-        {
-            return n1+n2;
-        }
-        else if(c == '-')
-        {
-            return n1-n2;
-        }
-        else if(c == '*')
-        {
-            return n1*n2;
-        }
-        else
-        {
-            return n1/n2;
-        }
-        
+        return stk.pop();
     }
 }
