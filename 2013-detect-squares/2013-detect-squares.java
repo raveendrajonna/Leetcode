f948880afbb64bc9c3c4f9a4af481ac228f84474
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-27 22:21:59 +0530

    Time: 307 ms (16.53%), Space: 105.8 MB (50.19%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,41 +1,34 @@
 class DetectSquares {
+
     List<List<Integer>> points;
-    HashMap<String, Integer> hs;
+    HashMap<String, Integer> hm;
     public DetectSquares() {
-        hs = new HashMap<>();
         points = new ArrayList<>();
+        hm = new HashMap<>();
     }
     
     public void add(int[] point) {
-       String key = point[0]+"@"+point[1];
-       hs.put(key, hs.getOrDefault(key,0)+1);
-       List<Integer> lst = new ArrayList<>();
-        lst.add(point[0]);
-        lst.add(point[1]);
-        points.add(lst);
+        List<Integer> p = new ArrayList<>();
+        p.add(point[0]);
+        p.add(point[1]);
+        points.add(p);
+        String key = point[0]+"@"+ point[1];
+        hm.put(key, hm.getOrDefault(key,0)+1);
     }
     
     public int count(int[] point) {
-        
         int x1 = point[0];
         int y1 = point[1];
         int res=0;
         for(int i=0; i<points.size(); i++)
         {
-            //Are thest diagnoals       
             int x2 = points.get(i).get(0);
             int y2 = points.get(i).get(1);
-            if(x1 == x2 || y1 == y2 )
+            
+            if(x1== x2 || y1 == y2 || ( Math.abs(x2-x1) != Math.abs(y1-y2)))
                 continue;
             
-            if( Math.abs(x2-x1) == Math.abs(y2-y1))
-            {
-                //x,y distances are same
-                
-                res =  res + (hs.getOrDefault(x1+"@"+y2,0) * hs.getOrDefault(x2+"@"+y1,0));
-                
-            }
-            
+            res = res + (hm.getOrDefault(x1+"@"+y2, 0) * hm.getOrDefault(x2+"@"+y1, 0));
         }
         return res;
     }
