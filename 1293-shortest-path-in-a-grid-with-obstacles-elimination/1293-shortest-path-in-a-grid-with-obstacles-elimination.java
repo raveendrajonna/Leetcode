5f68a855dbae8f6c38a0421db9812e34121e5aff
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-28 12:02:33 +0530

    Time: 45 ms (53.96%), Space: 58.4 MB (19.33%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,43 +1,41 @@
 class Solution {
-    
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        
+        int[][] dirArr = new int[][] { {-1,0} , {0,1} , { 1, 0} , {0, -1}};
         boolean[][][] visited = new boolean[m][n][k+1];
-        int[] ra = new int[] { -1, 0, 1, 0};
-        int[] ca = new int[] { 0, 1, 0, -1};
-        int res=0;
+        
         Queue<int[]> q = new LinkedList<>();
+        //0--> row, 1--> col, 2 --> step, 3-->obstacles;  
         q.add(new int[] { 0, 0, 0, 0});
         visited[0][0][0] = true;
-        
         while(q.size() > 0)
         {
             int[] cur = q.remove();
-            
-            if(cur[0] == m-1 && cur[1] == n-1)
-                return cur[2];
-                
-             for(int i=0; i<4; i++)
-             {
-                 int nr = cur[0]+ ra[i];
-                 int nc = cur[1] + ca[i];
-                 
-                 if(nr>= 0 && nr< m && nc>=0 && nc < n)
-                 {
-                     if(grid[nr][nc] == 0 && !visited[nr][nc][cur[3]])
-                     {
-                         visited[nr][nc][cur[3]] = true;
-                         q.add(new int[] { nr, nc, cur[2]+1, cur[3] });
-                     }
-                     else if( (cur[3]+1) <= k && !visited[nr][nc][cur[3]+1]) {
-                         visited[nr][nc][cur[3]+1] = true;
-                         q.add( new int[] { nr, nc, cur[2]+1, cur[3]+1});
-                     }
-                 }
-             }
-        }
-        return -1;
+            int r= cur[0];
+            int c = cur[1];        
+            if(r == m-1 && c == n-1)
+                return cur[2];                              
+            for(int i=0; i<4; i++)
+            {
+                int nr = r + dirArr[i][0];
+                int nc = c + dirArr[i][1];
+
+                if(nr>= 0 && nr < m && nc >=0 && nc <n)
+                {
+                    if(grid[nr][nc] == 0 && !visited[nr][nc][cur[3]])
+                    {
+                        q.add(new int[] { nr, nc, cur[2]+1, cur[3]});
+                        visited[nr][nc][cur[3]] = true;
+                    }
+                    if((cur[3]+1) <= k && !visited[nr][nc][cur[3]+1])
+                    {
+                        q.add(new int[] { nr, nc, cur[2]+1, cur[3]+1 });
+                        visited[nr][nc][cur[3]+1] = true;
+                    }
+                }
+            }
+            }
+      return -1;             
     }
 }
