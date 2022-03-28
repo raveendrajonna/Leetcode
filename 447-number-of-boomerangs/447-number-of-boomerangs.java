b65458b66900d016ff125fc8c81ec7dbb038da90
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-28 09:12:02 +0530

    Time: 273 ms (30.55%), Space: 117.3 MB (28.44%) - LeetHub

--- 447-number-of-boomerangs/447-number-of-boomerangs.java
+++ 447-number-of-boomerangs/447-number-of-boomerangs.java
@@ -1,31 +1,38 @@
 class Solution {
     public int numberOfBoomerangs(int[][] points) {
-      int n = points.length;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int res=0;
-        for(int i=0; i<n; i++)
+        int n = points.length;
+        int res =0;
+        
+        for(int i=0; i <n; i++)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(i == j)
-                    continue;
-                
-                int d = getDistance(points[i], points[j]);
-                hm.put(d, hm.getOrDefault(d,0)+1);
-            }
-            for(int val : hm.values())
-            {
-                res = res + (val * (val-1));
-            }
-            hm.clear();
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            
+            HashMap<Integer, Integer> hm = new HashMap<>();
+
+                for(int j=0; j<n; j++)
+                {
+                   if(i == j)
+                       continue;
+                   
+                   int  x2 = points[j][0];
+                   int  y2 = points[j][1];
+                   
+                    int dx = x2-x1;
+                    int dy = y2-y1;
+                    
+                    int d = (dx*dx) + (dy*dy);
+                    
+                    hm.put(d, hm.getOrDefault(d,0)+1);
+                }
+            
+               for(int d : hm.keySet())
+               {
+                   int cnt = hm.get(d);
+                   res = res + (cnt * (cnt-1) );
+               }
         }
+        
         return res;
-    }
-    
-    public int getDistance(int[] p1, int[] p2)
-    {
-        int dx = p1[0] - p2[0];
-        int dy = p1[1] - p2[1];
-        return (dx*dx)+ (dy*dy);
     }
 }
