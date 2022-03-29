23dcd9f6052e9a500ca3d3ba92e170e6e60be223
raveendrajonna <raveendra.jonna@gmail.com>
2022-03-29 23:17:14 +0530

    Time: 4 ms (99.72%), Space: 41.7 MB (96.86%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -1,34 +1,38 @@
 class Solution {
     public String rankTeams(String[] votes) {
-        int[][] dp = new int[26][26];
+        int[][] cnt = new int[26][26];
         
-        int n = votes.length;
         for(String word : votes)
         {
             for(int i=0; i<word.length(); i++)
             {
-                dp[word.charAt(i)-'A'][i]++;
+                cnt[word.charAt(i)-'A'][i]++;
             }
         }
         
-        char[] ans = votes[0].toCharArray();
+        char[] ans =  votes[0].toCharArray();
         Character[] temp = new Character[ans.length];
         for(int i=0; i<ans.length; i++)
         {
             temp[i] = ans[i];
         }
         
-        Arrays.sort(temp, (x, y) ->{
-            for(int i=0;i<26; i++)
-                if(dp[x-'A'][i] != dp[y-'A'][i]) 
-                    return dp[y-'A'][i] - dp[x-'A'][i];
-            return x-y;
+        Arrays.sort(temp, (x,y)->{
+           for(int i=0; i<26; i++)
+           {
+               if(cnt[x-'A'][i] != cnt[y-'A'][i])
+               {
+                   return cnt[y-'A'][i] - cnt[x-'A'][i];
+               }
+           }
+           return x-y;
         });
-            
-       for(int i=0; i<ans.length; i++)
+        
+        for(int i=0; i<ans.length; i++)
         {
             ans[i] = temp[i];
         }
-        return  new String(ans);
+        
+        return new String(ans);
     }
 }
